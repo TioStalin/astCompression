48bf3264b33ef4bb668963851863be1067a0d520
Daniel <33166073+TioStalin@users.noreply.github.com>
2020-11-23 15:49:30 -0300

    Cambio y mejora para Parseo

--- ASTparse.py
+++ ASTparse.py
@@ -1,21 +1,18 @@
 import json
+import os
 
-f = open('testing.json', 'r')
-data = json.load(f)
 
-cuerpo = data['ext'][0]['body']['block_items']
-declaracion = data['ext'][0]['decl']
 
 def getText(dict):
     if 'value' in dict.keys():
         return dict['value']
     elif 'name' in dict.keys():
-        return dict['name']
+        return 'var'
     elif '_nodetype' in dict.keys() and dict['_nodetype'] == 'UnaryOp':
-        if 'value' in dict.keys():
+        if 'value' in dict['expr'].keys():
             return f"{dict['op']}{dict['expr']['value']}"
-        elif 'name' in dict.keys():
-            return f"{dict['op']}{dict['expr']['name']}"
+        elif 'name' in dict['expr'].keys():
+            return f"{dict['op']}var"
 
 
 def printOperation(altura, tupla):
@@ -47,7 +44,10 @@ def BinOp(block):
 def blockItems(block, altura):
     for i in block:
         if i['_nodetype'] == 'Decl':
-            print(altura, i['type']['declname'])
+            if 'name' in i.keys():
+                print(altura, 'var')
+            else:
+                print(altura, i['type']['declname'])
         elif i['_nodetype'] == 'FuncCall':
             print(altura, i['name']['name'])
         elif i['_nodetype'] == 'Assignment':
@@ -84,13 +84,69 @@ def blockItems(block, altura):
             print(altura, 'return')
             print(altura+1, getText(i['expr']))
         elif i['_nodetype'] == 'UnaryOp':
-            print(altura, f"{i['expr']['name']}{i['op'][1:]}")
+            print(altura, f"var{i['op'][1:]}")
         elif i['_nodetype'] == 'Compound':
             blockItems(i['block_items'], altura)
+        elif i['_nodetype'] == 'Break':
+            print(altura, 'Break')
         else:
             print('No se encontro ninguna coincidencia')
             print(i)
 
-print('-1 EOF')
-print('0',declaracion['type']['type']['declname'])
-blockItems(cuerpo, 0)
+
+def getTreeList(file):
+    lista = []
+    with open(file, 'r') as fp:
+        for line in fp:
+            lista.append(line.strip().split(' '))
+    return lista
+
+def toTree(lista):
+    for i in range(len(lista)):
+        if i+1 == len(lista):
+            break
+        if int(lista[i][0]) == int(lista[i+1][0]):
+            print(lista[i][1] + ',', end="")
+        if int(lista[i][0]) < int(lista[i+1][0]):
+            print(lista[i][1] + '(', end="")
+        if int(lista[i][0]) > int(lista[i+1][0]):
+            print(lista[i][1], end="")
+            for j in range(int(lista[i][0]) - int(lista[i+1][0])):
+                print(')', end="")
+    print(lista[i][1], end="")
+    for j in range(abs(int(lista[i-1][0]) - int(lista[i][0]))):
+        print(')', end="")
+    print()
+
+f = open('./jsons/triangleNum.json', 'r')
+data = json.load(f)
+
+cuerpo = data['ext'][0]['body']['block_items']
+declaracion = data['ext'][0]['decl']
+aux = ''
+
+try:
+    for i in declaracion['type']['args']['params']:
+        aux += i['type']['type']['names'][0] + ','
+except:
+    print('No tiene parametros')
+
+# print('0', f'func[{aux}]')
+# blockItems(cuerpo, 0)
+
+
+lista_c = os.listdir('./ejemplos')
+print(len(lista_c))
+for i in range(len(lista_c)):
+    # with open(f'./txt/{lista_c[i]}') as fp:
+    #     # data = json.load(fp)
+    #     # cuerpo = data['ext'][0]['body']['block_items']
+    #     # declaracion = data['ext'][0]['decl']
+    #     # with open(f'./txt/{lista_c[i][:len(lista_c[i])-5]}.txt', 'w') as fp:
+    #         # string = f"0 {declaracion['type']['type']['declname']}"
+    #         # fp.write(string)
+    #     print(lista_c[i])
+    #     blockItems(cuerpo, 0)
+    #     print('\n\n\n')
+        lista = getTreeList(f'./ejemplos/{lista_c[i]}')
+        toTree(lista)
